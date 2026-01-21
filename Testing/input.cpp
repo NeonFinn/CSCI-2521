e10d7610f918f17491e969970bc342986f01a901
NeonFinn <n3onfinn@gmail.com>
2026-01-20 20:55:33 -0500

    lecture notes

---
+++ Testing/input.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int score1 = 0, score2 = 0, score3 = 0;
+    
+    cout << "Please enter Score #1: ";
+    cin >> score1;
+
+    cout << "Please enter Score #2: ";
+    cin >> score2;
+
+    cout << "Please enter Score #3: ";
+    cin >> score3;
+
+    cout << "Score 1: " << score1 << endl;
+    cout << "Score 2: " << score2 << endl;
+    cout << "Score 3: " << score3 << endl;
+
+    return 0;
+}

d9ed71ed77c1db6f58871c2427cca52a7c82e610
NeonFinn <n3onfinn@gmail.com>
2026-02-03 22:15:39 -0500

    comment code for readability

--- Lab 3/Lab3_fbaldwin3.cpp
+++ Lab 3/Lab3_fbaldwin3.cpp
@@ -19,13 +19,17 @@ int main()
     double circleArea;
     const double PI = 3.14159;
     
+    // show the user the menu options
     cout << "Menu" << endl;
     cout << "1. Calculate the Area of a Rectangle" << endl;
     cout << "2. Calculate the Area of a Circle" << endl;
     cout << "3. Quit" << endl;
     cout << "Please make a menu selection: ";
+
+    // get user input for menu selection
     cin >> userInput;
 
+    // if the player selects 1, prompt for length and width of a rectangle and solve for area
     if (userInput == "1")
     {
         cout << "Please enter the length of the rectangle: ";
@@ -40,6 +44,8 @@ int main()
 
         return 0;
     }
+
+    // if the player selects 2, prompt for radius of the circle and solve for area
     else if (userInput == "2")
     {
         cout << "Please enter the radius of the circle: ";
@@ -51,12 +57,16 @@ int main()
 
         return 0;
     }
+
+    // if the player selects 3, quit and tell them
     else if (userInput == "3")
     {
         cout << "You sucessfully quit the program.";
 
         return 0;
     }
+
+    // if the player makes an invalid choice, quit automatically
     else
     {
         cout << "You did not choose a valid menu option. Quitting the Program.";

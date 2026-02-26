69ecc4b181f3f9bf94e0489210192ab605252730
NeonFinn <n3onfinn@gmail.com>
2026-02-25 23:01:34 -0500

    more comments

--- Lab 6/Lab6_fbaldwin3.cpp
+++ Lab 6/Lab6_fbaldwin3.cpp
@@ -65,7 +65,7 @@ void arraySolution()
             cin >> score; 
         }
 
-        // once valid
+        // when valid put the score in the array
         scores[i] = score;
     }
 
@@ -74,6 +74,7 @@ void arraySolution()
     int highest = findMax(scores, 10);
     int lowest = findMin(scores, 10);
 
+    // display results after calculations
     cout << "\nArray Results:\n";
     cout << "Average: " << average << endl;
     cout << "Highest: " << highest << endl;
@@ -111,9 +112,11 @@ void vectorSolution()
             continue;
         }
 
+        // when valid push the score into the vector
         scores.push_back(score);
     }
 
+    // if no score was entered and the user inputs -1 to quit
     if (scores.size() == 0)
     {
         cout << "No scores entered.\n";
@@ -125,13 +128,16 @@ void vectorSolution()
     int highest = findMax(scores);
     int lowest = findMin(scores);
 
+    // display results after calculations
     cout << "\nVector Results:\n";
     cout << "Average score: " << average << endl;
     cout << "Highest score: " << highest << endl;
     cout << "Lowest score: " << lowest << endl;
 
+    // sort scores in vector
     sort(scores.begin(), scores.end());
 
+    // display sorted scores
     cout << "Sorted scores: ";
     for (int x : scores)
         cout << x << " ";
@@ -143,9 +149,9 @@ void vectorSolution()
 
 /**
  * @brief Calculates the average of integers in the an array
- * @param arr The array of integers
- * @param size Number of elements in the array
- * @return The average as a float
+ * @param arr array of integers
+ * @param size elements in the array
+ * @return average as a float
  */
 float calculateAverage(const int arr[], int size)
 {
@@ -157,9 +163,9 @@ float calculateAverage(const int arr[], int size)
 
 /**
  * @brief Finds the maximum value in an array
- * @param arr The array of integers
- * @param size Number of elements in the array
- * @return The maximum value
+ * @param arr array of integers
+ * @param size elements in the array
+ * @return maximum value
  */
 int findMax(const int arr[], int size)
 {
@@ -172,9 +178,9 @@ int findMax(const int arr[], int size)
 
 /**
  * @brief Finds the minimum value in an array
- * @param arr The array of integers
- * @param size Number of elements in the array
- * @return The minimum value
+ * @param arr array of integers
+ * @param size elements in the array
+ * @return minimum value
  */
 int findMin(const int arr[], int size)
 {
@@ -189,8 +195,8 @@ int findMin(const int arr[], int size)
 
 /**
  * @brief Calculates the average of the integers in the vector
- * @param vec The vector of integers
- * @return The average as a float
+ * @param vec vector of integers
+ * @return average as a float
  */
 float calculateAverage(const vector<int>& vec)
 {
@@ -202,8 +208,8 @@ float calculateAverage(const vector<int>& vec)
 
 /**
  * @brief Finds the maximum value in a vector
- * @param vec The vector of integers
- * @return The maximum value
+ * @param vec vector of integers
+ * @return maximum value
  */
 int findMax(const vector<int>& vec)
 {
@@ -216,8 +222,8 @@ int findMax(const vector<int>& vec)
 
 /**
  * @brief Finds the minimum value in a vector
- * @param vec The vector of integers
- * @return The minimum value
+ * @param vec vector of integers
+ * @return minimum value
  */
 int findMin(const vector<int>& vec)
 {
